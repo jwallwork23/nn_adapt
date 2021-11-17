a52eb24e9988f4348a35ac0f1140a6726290acfb
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-11-17 11:59:52 +0000

    metric: go_metric

--- nn_adapt/__init__.py
+++ nn_adapt/__init__.py
@@ -1,1 +1,3 @@
+from firedrake import *        # noqa
+
 from nn_adapt.metric import *  # noqa
--- nn_adapt/metric.py
+++ nn_adapt/metric.py
@@ -1,4 +1,4 @@
-from solving import *
+from nn_adapt.solving import *
 
 
 def get_hessians(f, **kwargs):
@@ -10,11 +10,54 @@ def get_hessians(f, **kwargs):
     ``recover_hessian``.
 
     :arg f: the function
-    :return: a dictionary containing the
-        nested structure of the Hessian
+    :return: list of Hessians of each
+        component
     """
-    return {[
-            hessian_metric(recover_hessian(fij, **kwargs))
-            for j, fij in enumerate(fi)
-        ] for i, fi in split_into_scalars(f).items()
-    }
+    kwargs.setdefault('method', 'L2')
+    return [
+        hessian_metric(recover_hessian(fij, **kwargs))
+        for i, fi in split_into_scalars(f).items()
+        for fij in fi
+    ]
+
+
+def go_metric(mesh, config, enrichment_method='h', target_complexity=4000.0,
+              average=True, interpolant='L2', retall=False):
+    """
+    Compute an anisotropic goal-oriented
+    metric field, based on a mesh and
+    a configuration file.
+
+    :arg mesh: input mesh
+    :arg config: configuration file, which
+        specifies the PDE and QoI
+    :kwarg enrichment_method: how to enrich the
+        finite element space?
+    :kwarg target_complexity: target complexity
+        of the goal-oriented metric
+    :kwarg average: should the Hessian components
+        be combined using averaging (or intersection)?
+    :kwarg interpolant: which method to use to
+        interpolate into the target space?
+    :kwarg retall: if ``True``, the error indicator,
+        forward solution, adjoint solution, enriched
+        adjoint solution and :class:`GoalOrientedMeshSeq`
+        are returned, in addition to the metric
+    """
+    dwr, fwd_sol, adj_sol, dwr_plus, adj_sol_plus, mesh_seq = indicate_errors(
+        mesh, config, enrichment_method=enrichment_method, retall=True
+    )
+    hessian = combine_metrics(
+        *get_hessians(fwd_sol),
+        *get_hessians(adj_sol),
+        average=average
+    )
+    metric = anisotropic_metric(
+        dwr, hessian, target_complexity=target_complexity,
+        target_space=TensorFunctionSpace(mesh, 'DG', 0),
+        interpolant=interpolant
+    )
+    if retall:
+        return metric, dwr, fwd_sol, adj_sol, dwr_plus, adj_sol_plus, mesh_seq
+    else:
+        return metric
