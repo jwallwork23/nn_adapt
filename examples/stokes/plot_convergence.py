bfd5739c018beb14d0d1e7841aec51d97ac309dc
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-11-18 11:41:47 +0000

    stokes: QoI convergence on same axes

--- examples/stokes/plot_convergence.py
+++ examples/stokes/plot_convergence.py
@@ -8,16 +8,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument('test_case', help='The configuration file number')
 test_case = int(parser.parse_args().test_case)
 assert test_case in [0, 1, 2, 3, 4]
-
-# Load data
-dofs = np.load(f'data/dofs_uniform{test_case}.npy')
-qois = np.load(f'data/qois_uniform{test_case}.npy')
+labels = ['Uniform refinement', 'Goal-oriented adaptation']
 
 # Plot
 fig, axes = plt.subplots()
-axes.semilogx(dofs, qois, '--x')
+for approach, label in zip(['uniform', 'go'], labels):
+    try:
+        dofs = np.load(f'data/dofs_{approach}{test_case}.npy')
+        qois = np.load(f'data/qois_{approach}{test_case}.npy')
+    except IOError:
+        print(f'Cannot load {approach} data for test case {test_case}')
+        continue
+    axes.semilogx(dofs, qois, '--x', label=label)
 axes.set_xlabel('DoF count')
 axes.set_ylabel('Quantity of Interest')
 axes.grid(True)
+axes.legend()
 plt.tight_layout()
-plt.savefig(f'plots/uniform_convergence{test_case}.pdf')
+plt.savefig(f'plots/qoi_convergence{test_case}.pdf')
