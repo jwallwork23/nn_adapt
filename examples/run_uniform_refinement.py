27cbd6d6ae35508d0fafc78e5fa14076a48bf3f5
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-11-18 16:55:44 +0000

    examples: generalise run_uniform_refinement

--- examples/run_uniform_refinement.py
+++ examples/run_uniform_refinement.py
@@ -7,16 +7,19 @@ import os
 
 # Parse for test case and number of refinements
 parser = argparse.ArgumentParser()
+parser.add_argument('model', help='The model')
 parser.add_argument('test_case', help='The configuration file number')
 parser.add_argument('-num_refinements', help='Number of mesh refinements')
 parsed_args = parser.parse_args()
+model = parsed_args.model
+assert model in ['stokes']
 test_case = int(parsed_args.test_case)
 assert test_case in [0, 1, 2, 3, 4]
 num_refinements = int(parsed_args.num_refinements or 5)
 assert num_refinements >= 0
 
 # Setup
-setup = importlib.import_module(f'config{test_case}')
+setup = importlib.import_module(f'{model}.config{test_case}')
 field = setup.fields[0]
 
 # Run uniform refinement
@@ -25,7 +28,7 @@ dofs = []
 elements = []
 print(f'Test case {test_case}')
 for i in range(num_refinements+1):
-    mesh = Mesh(os.path.join(os.path.abspath(os.path.dirname(__file__)), f'meshes/{test_case}.{i}.msh'))
+    mesh = Mesh(f'{os.path.abspath(os.path.dirname(__file__)}/{model}/meshes/{test_case}.{i}.msh'))
     print(f'  Mesh {i}')
     print(f'    Element count        = {mesh.num_cells()}')
     fwd_sol, mesh_seq = get_solutions(mesh, setup, adjoint=False)
@@ -35,6 +38,6 @@ for i in range(num_refinements+1):
     qois.append(qoi)
     dofs.append(sum(fs.dof_count))
     elements.append(mesh.num_cells())
-    np.save(f'data/qois_uniform{test_case}', qois)
-    np.save(f'data/dofs_uniform{test_case}', dofs)
-    np.save(f'data/elements_uniform{test_case}', elements)
+    np.save(f'{model}/data/qois_uniform{test_case}', qois)
+    np.save(f'{model}/data/dofs_uniform{test_case}', dofs)
+    np.save(f'{model}/data/elements_uniform{test_case}', elements)
