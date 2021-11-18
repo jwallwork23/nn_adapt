11687d32e1820c3f7b19da78dd8fb911d61c2ea1
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-11-18 16:46:21 +0000

    examples: generalise plot_convergence

--- examples/plot_convergence.py
+++ examples/plot_convergence.py
@@ -5,8 +5,12 @@ import numpy as np
 
 # Parse for test case
 parser = argparse.ArgumentParser()
+parser.add_argument('model', help='The model')
 parser.add_argument('test_case', help='The configuration file number')
-test_case = int(parser.parse_args().test_case)
+parsed_args = parser.parse_args()
+model = parsed_args.model
+assert model in ['stokes']
+test_case = int(parsed_args.test_case)
 assert test_case in [0, 1, 2, 3, 4]
 labels = ['Uniform refinement', 'Goal-oriented adaptation']
 
@@ -14,8 +18,8 @@ labels = ['Uniform refinement', 'Goal-oriented adaptation']
 fig, axes = plt.subplots()
 for approach, label in zip(['uniform', 'go'], labels):
     try:
-        dofs = np.load(f'data/dofs_{approach}{test_case}.npy')
-        qois = np.load(f'data/qois_{approach}{test_case}.npy')
+        dofs = np.load(f'{model}/data/dofs_{approach}{test_case}.npy')
+        qois = np.load(f'{model}/data/qois_{approach}{test_case}.npy')
     except IOError:
         print(f'Cannot load {approach} data for test case {test_case}')
         continue
@@ -25,4 +29,4 @@ axes.set_ylabel('Quantity of Interest')
 axes.grid(True)
 axes.legend()
 plt.tight_layout()
-plt.savefig(f'plots/qoi_convergence{test_case}.pdf')
+plt.savefig(f'{model}/plots/qoi_convergence{test_case}.pdf')
