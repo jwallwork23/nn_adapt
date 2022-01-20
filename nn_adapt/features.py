c3192288eaefd2860886467cea4b5dd35b8f4059
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-01-20 15:01:14 +0000

    features: combine using hstack and vstack

--- nn_adapt/features.py
+++ nn_adapt/features.py
@@ -26,17 +26,9 @@ def extract_components(matrix):
     armin = ar.vector().gather().min()
     assert armin >= 1.0, f'An element has aspect ratio is less than one ({armin})'
     theta = firedrake.interpolate(ufl.atan(evecs[1, 1]/evecs[1, 0]), fs)
-    # return density.dat.data, ar.dat.data, theta.dat.data
     h1 = firedrake.interpolate(ufl.cos(theta)**2/ar + ufl.sin(theta)**2*ar, fs)
     h2 = firedrake.interpolate((1/ar - ar)*ufl.sin(theta)*ufl.cos(theta), fs)
     return density.dat.data, h1.dat.data, h2.dat.data
-    # H = firedrake.interpolate(ufl.as_matrix([[h1, h2], [h2, h1]]), matrix.function_space())
-    # V, Lambda = compute_eigendecomposition(H)
-    # lmin = Lambda.vector().gather().min()
-    # assert lmin > 0.0, f'Normalised Hessian is not positive-definite (min eigenvalue {lmin})'
-    # Lambda.dat.data[:] = np.log(Lambda.dat.data)
-    # logH = assemble_eigendecomposition(V, Lambda)
-    # return density.dat.data, logH.dat.data[:, 0, 0], logH.dat.data[:, 0, 1]
 
 
 @PETSc.Log.EventDecorator("Extract elementwise")
@@ -109,14 +101,7 @@ def extract_features(config, fwd_sol, adj_sol, preproc='none'):
 
     # Combine the features together
     with PETSc.Log.Event('Combine features'):
-        num_inputs = config.parameters.num_inputs
-        features = np.array([]).reshape(0, num_inputs)
-        for i in range(mesh.num_cells()):
-            mesh_features = [d[i], h1[i], h2[i], bnd_tags[i]]
-            solution_features = [vv for v in vals for vv in v[i]]
-            feature = [Re[i]] + mesh_features + solution_features
-            feature = np.reshape(feature, (1, num_inputs))
-            features = np.concatenate((features, feature))
+        features = np.hstack((np.vstack([Re, d, h1, h2, bnd_tags]).transpose(), np.hstack(vals)))
     assert not np.isnan(features).any()
     return preprocess_features(features, preproc=preproc)
 
