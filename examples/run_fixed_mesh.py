023fadca90f0c0d773885e3ab25e9a0574e87573
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-01-20 10:46:21 +0000

    examples: time fixed mesh

--- examples/run_fixed_mesh.py
+++ examples/run_fixed_mesh.py
@@ -1,24 +1,40 @@
 from nn_adapt import *
+from firedrake.petsc import PETSc
+
 import argparse
 import importlib
 import os
+from time import perf_counter
 
+
+start_time = perf_counter()
 
 # Parse for test case
 parser = argparse.ArgumentParser()
 parser.add_argument('model', help='The model')
 parser.add_argument('test_case', help='The configuration file number')
-parsed_args = parser.parse_args()
+parser.add_argument('-num_refinements', help='Number of mesh refinements')
+parser.add_argument('-optimise', help='Turn off plotting and debugging (default False)')
+parsed_args, unknown_args = parser.parse_known_args()
 model = parsed_args.model
 assert model in ['stokes', 'turbine']
 test_case = int(parsed_args.test_case)
 assert test_case in list(range(12))
+num_refinements = int(parsed_args.num_refinements or 0)
+assert num_refinements >= 0
+optimise = bool(parsed_args.optimise or False)
 
 # Run fixed mesh setup
 setup = importlib.import_module(f'{model}.config{test_case}')
 mesh = Mesh(f'{os.path.abspath(os.path.dirname(__file__))}/{model}/meshes/{test_case}.msh')
+if num_refinements > 0:
+    with PETSc.Log.Event('Hierarchy'):
+        mesh = MeshHierarchy(mesh, num_refinements)[-1]
+
 fwd_sol, adj_sol = get_solutions(mesh, setup)
 J = assemble(setup.get_qoi(mesh)(fwd_sol))
 print(f'QoI for test case {test_case} = {J:.2f} MW')
-File(f'{model}/outputs/fixed/forward{test_case}.pvd').write(*fwd_sol.split())
-File(f'{model}/outputs/fixed/adjoint{test_case}.pvd').write(*adj_sol.split())
+if not optimise:
+    File(f'{model}/outputs/fixed/forward{test_case}.pvd').write(*fwd_sol.split())
+    File(f'{model}/outputs/fixed/adjoint{test_case}.pvd').write(*adj_sol.split())
+print(f'  Total time taken: {perf_counter() - start_time:.2f} seconds')
