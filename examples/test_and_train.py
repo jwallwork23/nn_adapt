56708974245862d99679d941410a7b7fae05e744
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-01-21 08:48:01 +0000

    examples: adaptive learning rate

--- examples/test_and_train.py
+++ examples/test_and_train.py
@@ -1,13 +1,17 @@
-import argparse
-from sklearn import model_selection
 from nn_adapt.ann import *
 from nn_adapt.features import preprocess_features
+
+import argparse
+from sklearn import model_selection
+from torch.optim.lr_scheduler import StepLR
 
 
 # Configuration
 parser = argparse.ArgumentParser(prog='test_and_train.py')
 parser.add_argument('model', help='The equation set being solved')
-parser.add_argument('-learning_rate', help='The step length (default 2.0e-03)')
+parser.add_argument('-lr', help='Starting learning rate, i.e. step length (default 2.0e-03)')
+parser.add_argument('-lr_adapt_num_steps', help='Number of steps between learning rate adapts (default 100)')
+parser.add_argument('-lr_adapt_factor', help='Factor by which to reduce the learning rate (default 0.8)')
 parser.add_argument('-num_epochs', help='The number of iterations (default 1000)')
 parser.add_argument('-preproc', help='Function for preprocessing data (default "arctan")')
 parser.add_argument('-batch_size', help='Number of data points per training iteration (default 1000)')
@@ -16,7 +20,12 @@ parser.add_argument('-test_size', help='Proportion of data used for validation (
 parsed_args = parser.parse_args()
 model = parsed_args.model
 assert model in ['stokes', 'turbine']
-lr = float(parsed_args.learning_rate or 2.0e-03)
+lr = float(parsed_args.lr or 2.0e-03)
+assert lr > 0.0
+lr_adapt_num_steps = int(parsed_args.lr_adapt_num_steps or 100)
+assert lr_adapt_num_steps > 0
+lr_adapt_factor = float(parsed_args.lr_adapt_factor or 0.8)
+assert 0.0 < lr_adapt_factor < 1.0
 num_epochs = int(parsed_args.num_epochs or 1000)
 preproc = parsed_args.preproc or 'arctan'
 batch_size = int(parsed_args.batch_size or 1000)
@@ -64,6 +73,7 @@ def Loss():
 # Setup model
 nn = SimpleNet().to(device)
 optimizer = torch.optim.Adam(nn.parameters(), lr=lr)
+scheduler = StepLR(optimizer, lr_adapt_num_steps, gamma=lr_adapt_factor)
 criterion = Loss()
 print(f"Model parameters are{'' if all(p.is_cuda for p in nn.parameters()) else ' not'} using GPU cores.")
 
@@ -86,10 +96,13 @@ for epoch in range(num_epochs):
 
     # Stash progreess
     print(f"Epoch {epoch:4d}/{num_epochs:d}"
-          f"  avg loss: {train:.4e}/{val:.4e}"
-          f"  wallclock: {train_time:.2f}s/{validation_time:.2f}s")
+          f"  avg loss: {train:.4e} / {val:.4e}"
+          f"  wallclock: {train_time:.2f}s / {validation_time:.2f}s")
     train_losses.append(train)
     validation_losses.append(val)
     np.save(f'{model}/data/train_losses', train_losses)
     np.save(f'{model}/data/validation_losses', validation_losses)
     torch.save(nn.state_dict(), f'{model}/model.pt')
+
+    # Adapt learning rate
+    scheduler.step()
