2a29254e7a6038f0f447b135b3bfbcb4ec0886bc
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-01-26 17:18:17 +0000

    turbine: random locations

--- examples/turbine/config.py
+++ examples/turbine/config.py
@@ -1,9 +1,15 @@
 from models.turbine import *
 
 
+def l2dist(xy, xyt):
+    diff = np.array(xy) - np.array(xyt)
+    return np.sqrt(np.dot(diff, diff))
+
+
 def initialise(i):
     if i < 14:
-        np.random.seed(3*i)
+        parameters.turbine_coords = []
+        np.random.seed(100*i)
 
         # Random depth from 20m to 100m
         parameters.depth = 20.0 + 80.0*np.random.rand()
@@ -13,30 +19,25 @@ def initialise(i):
         exponent = np.random.randint(-3, 1)
         parameters.viscosity.assign(significand*10**exponent)
 
+        # Randomise turbine configuration
+        num_turbines = np.random.randint(1, 8)
+        tc = parameters.turbine_coords
+        i = 0
+        while i < num_turbines:
+            x = 50.0 + 1100.0*np.random.rand()
+            y = 50.0 + 400.0*np.random.rand()
+            valid = True
+            for xyt in tc:
+                if l2dist((x, y), xyt) < 50.0:
+                    valid = False
+            if valid:
+                tc.append((x, y))
+                i += 1
+
     elif i in [14, 15]:
         parameters.viscosity.assign(0.5)
         parameters.depth = 40.0
-
-    # Set coordinates
-    parameters.turbine_coords = [
-
-        # Training and validation
-        [(600, 250)],
-        [(600, 375)],
-        [(50, 250)],
-        [(1100, 250)],
-        [(206, 300), (796, 300)],
-        [(456, 220), (456, 280)],
-        [(256, 250), (544, 250)],
-        [(456, 44), (456, 456)],
-        [(400, 268), (600, 250), (800, 232)],
-        [(420, 250), (600, 232), (780, 250)],
-        [(650, 400), (700, 400), (750, 400)],
-        [(100, 450), (600, 250), (1100, 50)],
-        [(500, 100), (500, 200), (500, 300), (500, 400)],
-        [(500, 286), (600, 250), (700, 214), (500, 214), (700, 286)],
-
-        # Testing
-        [(456, 250), (744, 250)],  # aligned
-        [(456, 232), (744, 268)],  # offset
-    ][i]
+        if i == 14:
+            parameters.turbine_coords = [(456, 250), (744, 250)]  # aligned
+        else:
+            parameters.turbine_coords = [(456, 232), (744, 268)]  # offset
