c2dd06cd1e708b6de28cac727df1f4e4245e8409
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-01 10:08:05 +0000

    examples: clean up run_fixed_mesh

--- examples/run_fixed_mesh.py
+++ examples/run_fixed_mesh.py
@@ -1,10 +1,9 @@
 """
 Run a given ``test_case`` of a ``model`` on the initial mesh alone.
 """
+from nn_adapt.parse import *
 from nn_adapt.solving import *
 from firedrake.petsc import PETSc
-
-import argparse
 import importlib
 from time import perf_counter
 
@@ -12,11 +11,14 @@ from time import perf_counter
 start_time = perf_counter()
 
 # Parse for test case
-parser = argparse.ArgumentParser(prog="run_fixed_mesh.py")
-parser.add_argument("model", help="The model")
-parser.add_argument("test_case", help="The configuration file number")
-parser.add_argument("-num_refinements", help="Number of mesh refinements")
-parser.add_argument("-optimise", help="Turn off plotting and debugging (default False)")
+parser = argparse.ArgumentParser(
+    prog="run_fixed_mesh.py",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("model", help="The model", type=str)
+parser.add_argument("test_case", help="The configuration file number or name")
+parser.add_argument("--num_refinements", help="Number of mesh refinements", type=positive_int, default=0)
+parser.add_argument("--optimise", help="Turn off plotting and debugging", action="store_true")
 parsed_args, unknown_args = parser.parse_known_args()
 model = parsed_args.model
 try:
@@ -24,28 +26,26 @@ try:
     assert test_case > 0
 except ValueError:
     test_case = parsed_args.test_case
-num_refinements = int(parsed_args.num_refinements or 0)
-assert num_refinements >= 0
-optimise = bool(parsed_args.optimise or False)
 
 # Setup
 setup = importlib.import_module(f"{model}.config")
 setup.initialise(test_case)
 unit = setup.parameters.qoi_unit
 mesh = Mesh(f"{model}/meshes/{test_case}.msh")
-if num_refinements > 0:
+if parsed_args.num_refinements > 0:
     with PETSc.Log.Event("Hierarchy"):
-        mesh = MeshHierarchy(mesh, num_refinements)[-1]
+        mesh = MeshHierarchy(mesh, parsed_args.num_refinements)[-1]
 
 # Solve and evaluate QoI
-sols = get_solutions(mesh, setup, solve_adjoint=not optimise)
-if optimise:
+sols = get_solutions(mesh, setup, solve_adjoint=not parsed_args.optimise)
+msg = f"QoI for test case {test_case}"
+if parsed_args.optimise:
     print(
-        f"QoI for test case {test_case} = {assemble(setup.get_qoi(mesh)(sols)):.2f} {unit}"
+        f"{msg} = {assemble(setup.get_qoi(mesh)(sols)):.2f} {unit}"
     )
 else:
     print(
-        f"QoI for test case {test_case} = {assemble(setup.get_qoi(mesh)(sols[0])):.2f} {unit}"
+        f"{msg} = {assemble(setup.get_qoi(mesh)(sols[0])):.2f} {unit}"
     )
     File(f"{model}/outputs/{test_case}/fixed/forward.pvd").write(*sols[0].split())
     File(f"{model}/outputs/{test_case}/fixed/adjoint.pvd").write(*sols[1].split())
