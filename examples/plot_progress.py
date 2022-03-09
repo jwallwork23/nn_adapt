158af6a82a1c595f59fdea8fc8da927a749917f2
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-09 11:02:01 +0000

    examples: update plot_progress

--- examples/plot_progress.py
+++ examples/plot_progress.py
@@ -2,30 +2,43 @@
 Plot the training and validation loss curves for a network
 trained on a particular ``model``.
 """
+from nn_adapt.parse import argparse, positive_int
 from nn_adapt.plotting import *
 
-import argparse
+import git
 import numpy as np
 
 
 # Parse model
-parser = argparse.ArgumentParser(prog="plot_progress.py")
-parser.add_argument("model", help="The equation set being solved")
-parser.add_argument("-num_epochs", help="The number of iterations (default 1000)")
-parser.add_argument("-git_sha", help="Git commit sha (defaults to current)")
+parser = argparse.ArgumentParser(
+    prog="plot_progress.py",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "model",
+    help="The model",
+    type=str,
+    choices=["turbine"],
+)
+parser.add_argument(
+    "--num_epochs",
+    help="The number of iterations",
+    type=positive_int,
+    default=1000,
+)
+parser.add_argument(
+    "--tag",
+    help="Model tag (defaults to current git commit sha)",
+    default=None,
+)
 parsed_args = parser.parse_args()
 model = parsed_args.model
-num_epochs = int(parsed_args.num_epochs or 1000)
-assert num_epochs > 0
-sha = parsed_args.git_sha
-if sha is None:
-    import git
-
-    sha = git.Repo(search_parent_directories=True).head.object.hexsha
+num_epochs = parsed_args.num_epochs
+tag = parsed_args.tag or git.Repo(search_parent_directories=True).head.object.hexsha
 
 # Load data
-train_losses = np.load(f"{model}/data/train_losses_{sha}.npy")
-validation_losses = np.load(f"{model}/data/validation_losses_{sha}.npy")
+train_losses = np.load(f"{model}/data/train_losses_{tag}.npy")
+validation_losses = np.load(f"{model}/data/validation_losses_{tag}.npy")
 epochs = np.arange(len(train_losses)) + 1
 
 # Plot training losses
