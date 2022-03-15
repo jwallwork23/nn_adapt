8a08a9152241ff0ce96b4d86e372462080de1ffe
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-15 15:45:10 +0000

    examples: plot_convergence for times

--- examples/plot_convergence.py
+++ examples/plot_convergence.py
@@ -9,23 +9,18 @@ from nn_adapt.plotting import *
 import importlib
 from matplotlib.ticker import FormatStrFormatter
 import numpy as np
+import os
+import sys
 
-
-def plot_slope(x0, x1, y0, g, axes):
-    """
-    Plot a slope marker for the gradient of a curve.
-    """
-    y1 = y0 * (x1 / x0) ** g
-    axes.plot([x0, x1], [y0, y1], "-", color="darkgray")
-
-
-matplotlib.rcParams["font.size"] = 20
 
 # Parse user input
 parser = Parser("plot_convergence.py")
 parsed_args = parser.parse_args()
 model = parsed_args.model
 test_case = parsed_args.test_case
+
+# Formatting
+matplotlib.rcParams["font.size"] = 20
 approaches = {
     "uniform": {
         "label": "Uniform refinement",
@@ -58,32 +53,43 @@ approaches = {
         "linestyle": "--",
     },
 }
-xlim = [3.0e03, 4.0e06]
+xlim = {
+    "dofs": [3.0e03, 4.0e06],
+    "elements": [3.0e02, 4.0e05],
+    "times": [1.0e0, 1.0e03],
+}
 
 # Load configuration
 setup = importlib.import_module(f"{model}.config")
 unit = setup.parameters.qoi_unit
 qoi_name = setup.parameters.qoi_name.capitalize()
 
-dofs, elements, qois = {}, {}, {}
+# Load outputs
+dofs, elements, qois, times = {}, {}, {}, {}
 for approach in approaches:
     try:
         dofs[approach] = np.load(f"{model}/data/dofs_{approach}_{test_case}.npy")
-        elements[approach] = np.load(f"{model}/data/elements_{approach}_{test_case}.npy")
+        elements[approach] = np.load(
+            f"{model}/data/elements_{approach}_{test_case}.npy"
+        )
         qois[approach] = np.load(f"{model}/data/qois_{approach}_{test_case}.npy")
+        times[approach] = np.load(f"{model}/data/times_{approach}_{test_case}.npy")
     except IOError:
         print(f"Cannot load {approach} data for test case {test_case}")
         approaches.pop(approach)
         continue
+if len(approaches.keys()) == 0:
+    print("Nothing to plot.")
+    sys.exit(0)
 
 # Plot QoI curves against DoF count
 fig, axes = plt.subplots()
 start = max(np.load(f"{model}/data/qois_uniform_{test_case}.npy"))
 conv = np.load(f"{model}/data/qois_GOanisotropic_{test_case}.npy")[-1]
-axes.hlines(conv, *xlim, "k", label="Converged QoI")
+axes.hlines(conv, *xlim["dofs"], "k", label="Converged QoI")
 for approach, metadata in approaches.items():
     axes.semilogx(dofs[approach], qois[approach], **metadata)
-axes.set_xlim(xlim)
+axes.set_xlim(xlim["dofs"])
 axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
 axes.yaxis.set_major_formatter(FormatStrFormatter("%.2f"))
 axes.set_xlabel("DoF count")
@@ -93,28 +99,46 @@ plt.tight_layout()
 plt.savefig(f"{model}/plots/qoi_vs_dofs_{test_case}.pdf")
 
 # Plot legend
-fig2, axes2 = plt.subplots()
-lines, labels = axes.get_legend_handles_labels()
-legend = axes2.legend(lines, labels, frameon=False, ncol=3)
-fig2.canvas.draw()
-axes2.set_axis_off()
-bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
-plt.savefig(f"{model}/plots/legend.pdf", bbox_inches=bbox)
+fname = f"{model}/plots/legend.pdf"
+if not os.path.exists(fname):
+    fig2, axes2 = plt.subplots()
+    lines, labels = axes.get_legend_handles_labels()
+    legend = axes2.legend(lines, labels, frameon=False, ncol=3)
+    fig2.canvas.draw()
+    axes2.set_axis_off()
+    bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
+    plt.savefig(fname, bbox_inches=bbox)
 
 # Plot QoI curves against element count
 fig, axes = plt.subplots()
-start = max(np.load(f"{model}/data/qois_uniform_{test_case}.npy"))
-conv = np.load(f"{model}/data/qois_GOanisotropic_{test_case}.npy")[-1]
-axes.hlines(conv, *xlim, "k", label="Converged QoI")
+start = max(qois["uniform"])
+conv = qois["GOanisotropic"][-1]
+axes.hlines(conv, *xlim["elements"], "k", label="Converged QoI")
 for approach, metadata in approaches.items():
     axes.semilogx(elements[approach], qois[approach], **metadata)
+axes.set_xlim(xlim["elements"])
 axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
 axes.yaxis.set_major_formatter(FormatStrFormatter("%.2f"))
 axes.set_xlabel("Element count")
-axes.set_ylabel(qoi_name + r" output ($\mathrm{" + unit + "}$)")
+axes.set_ylabel(qoi_name + r" ($\mathrm{" + unit + "}$)")
 axes.grid(True)
 plt.tight_layout()
 plt.savefig(f"{model}/plots/qoi_vs_elements_{test_case}.pdf")
+plt.close()
+
+# Plot QoI curves against CPU time
+fig, axes = plt.subplots()
+axes.hlines(conv, *xlim["times"], "k", label="Converged QoI")
+for approach, metadata in approaches.items():
+    axes.semilogx(times[approach], qois[approach], **metadata)
+axes.set_xlim(xlim["times"])
+axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
+axes.yaxis.set_major_formatter(FormatStrFormatter("%.2f"))
+axes.set_xlabel(r"CPU time ($\mathrm{s}$)")
+axes.set_ylabel(qoi_name + r" ($\mathrm{" + unit + "}$)")
+axes.grid(True)
+plt.tight_layout()
+plt.savefig(f"{model}/plots/qoi_vs_cputime_{test_case}.pdf")
 plt.close()
 
 # Plot QoI error curves against DoF count
@@ -125,7 +149,7 @@ for approach, metadata in approaches.items():
     d = dofs[approach][:-1] if approach == "GOanisotropic" else dofs[approach]
     err = errors[approach][:-1] if approach == "GOanisotropic" else errors[approach]
     axes.loglog(d, err, **metadata)
-axes.set_xlim(xlim)
+axes.set_xlim(xlim["dofs"])
 axes.set_xlabel("DoF count")
 axes.set_ylabel(r"QoI error ($\%$)")
 axes.grid(True, which="both")
@@ -139,9 +163,24 @@ for approach, metadata in approaches.items():
     els = elements[approach][:-1] if approach == "GOanisotropic" else elements[approach]
     err = errors[approach][:-1] if approach == "GOanisotropic" else errors[approach]
     axes.loglog(els, err, **metadata)
+axes.set_xlim(xlim["elements"])
 axes.set_xlabel("Element count")
 axes.set_ylabel(r"QoI error ($\%$)")
 axes.grid(True, which="both")
 plt.tight_layout()
 plt.savefig(f"{model}/plots/qoi_error_vs_elements_{test_case}.pdf")
+plt.close()
+
+# Plot QoI error curves against CPU time
+fig, axes = plt.subplots()
+for approach, metadata in approaches.items():
+    t = times[approach][:-1] if approach == "GOanisotropic" else times[approach]
+    err = errors[approach][:-1] if approach == "GOanisotropic" else errors[approach]
+    axes.loglog(t, err, **metadata)
+axes.set_xlim(xlim["times"])
+axes.set_xlabel(r"CPU time ($\mathrm{s}$)")
+axes.set_ylabel(r"QoI error ($\%$)")
+axes.grid(True, which="both")
+plt.tight_layout()
+plt.savefig(f"{model}/plots/qoi_error_vs_cputime_{test_case}.pdf")
 plt.close()
