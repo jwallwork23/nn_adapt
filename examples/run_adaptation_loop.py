b456059f6a3ba0a26539be9c7ea92c435b1278eb
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-06-16 08:58:39 +0100

    examples: skip upon conv error

--- examples/run_adaptation_loop.py
+++ examples/run_adaptation_loop.py
@@ -46,84 +46,88 @@ unit = setup.parameters.qoi_unit
 qois, dofs, elements, estimators, times, niter = [], [], [], [], [], []
 print(f"Test case {test_case}")
 for i in range(num_refinements + 1):
-    target_complexity = 200.0 * 4**i
-    kwargs = {
-        "enrichment_method": "h",
-        "average": False,
-        "anisotropic": approach == "anisotropic",
-        "retall": True,
-    }
-    mesh = Mesh(f"{model}/meshes/{test_case}.msh")
-    qoi_old = None
-    elements_old = mesh.num_cells()
-    estimator_old = None
-    converged_reason = None
-    print(f"  Target {target_complexity}\n    Mesh 0")
-    print(f"      Element count        = {elements_old}")
-    cpu_timestamp = perf_counter()
-    for fp_iteration in range(maxiter + 1):
-
-        # Ramp up the target complexity
-        target_ramp = ramp_complexity(200.0, target_complexity, fp_iteration)
-        kwargs["target_complexity"] = target_ramp
-
-        # Compute goal-oriented metric
-        p0metric, dwr, fwd_sol, adj_sol = go_metric(mesh, setup, **kwargs)
-        # TODO: break this up for optimising timings
-        dof = sum(fwd_sol.function_space().dof_count)
-        print(f"      DoF count            = {dof}")
-
-        # Check for QoI convergence
-        qoi = assemble(setup.get_qoi(mesh)(fwd_sol))
-        print(f"      Quantity of Interest = {qoi} {unit}")
-        if qoi_old is not None and fp_iteration >= miniter:
-            if abs(qoi - qoi_old) < qoi_rtol * abs(qoi_old):
-                converged_reason = "QoI convergence"
-                break
-        qoi_old = qoi
-
-        # Check for error estimator convergence
-        estimator = dwr.vector().gather().sum()
-        print(f"      Error estimator      = {estimator}")
-        if estimator_old is not None and fp_iteration >= miniter:
-            if abs(estimator - estimator_old) < estimator_rtol * abs(estimator_old):
-                converged_reason = "error estimator convergence"
-                break
-        estimator_old = estimator
-
-        # Process metric
-        P1_ten = TensorFunctionSpace(mesh, "CG", 1)
-        p1metric = hessian_metric(clement_interpolant(p0metric))
-        space_normalise(p1metric, target_ramp, "inf")
-        enforce_element_constraints(
-            p1metric, setup.parameters.h_min, setup.parameters.h_max, 1.0e05
-        )
-
-        # Adapt the mesh and check for element count convergence
-        metric = RiemannianMetric(mesh)
-        metric.assign(p1metric)
-        mesh = adapt(mesh, metric)
-        print(f"    Mesh {fp_iteration+1}")
-        if fp_iteration >= miniter:
-            if abs(mesh.num_cells() - elements_old) < element_rtol * abs(elements_old):
-                converged_reason = "element count convergence"
-                break
+    try:
+        target_complexity = 200.0 * 4**i
+        kwargs = {
+            "enrichment_method": "h",
+            "average": False,
+            "anisotropic": approach == "anisotropic",
+            "retall": True,
+        }
+        mesh = Mesh(f"{model}/meshes/{test_case}.msh")
+        qoi_old = None
         elements_old = mesh.num_cells()
+        estimator_old = None
+        converged_reason = None
+        print(f"  Target {target_complexity}\n    Mesh 0")
         print(f"      Element count        = {elements_old}")
+        cpu_timestamp = perf_counter()
+        for fp_iteration in range(maxiter + 1):
 
-        # Check for reaching maximum number of iterations
-        if fp_iteration == maxiter:
-            converged_reason = "reaching maximum iteration count"
-    print(f"    Terminated after {fp_iteration+1} iterations due to {converged_reason}")
-    times.append(perf_counter() - cpu_timestamp)
-    qois.append(qoi)
-    dofs.append(dof)
-    elements.append(elements_old)
-    estimators.append(estimator)
-    niter.append(fp_iteration + 1)
-    np.save(f"{model}/data/qois_GO{approach}_{test_case}", qois)
-    np.save(f"{model}/data/dofs_GO{approach}_{test_case}", dofs)
-    np.save(f"{model}/data/elements_GO{approach}_{test_case}", elements)
-    np.save(f"{model}/data/estimators_GO{approach}_{test_case}", estimators)
-    np.save(f"{model}/data/times_GO{approach}_{test_case}", times)
-    np.save(f"{model}/data/niter_GO{approach}_{test_case}", niter)
+            # Ramp up the target complexity
+            target_ramp = ramp_complexity(200.0, target_complexity, fp_iteration)
+            kwargs["target_complexity"] = target_ramp
+
+            # Compute goal-oriented metric
+            p0metric, dwr, fwd_sol, adj_sol = go_metric(mesh, setup, **kwargs)
+            # TODO: break this up for optimising timings
+            dof = sum(fwd_sol.function_space().dof_count)
+            print(f"      DoF count            = {dof}")
+
+            # Check for QoI convergence
+            qoi = assemble(setup.get_qoi(mesh)(fwd_sol))
+            print(f"      Quantity of Interest = {qoi} {unit}")
+            if qoi_old is not None and fp_iteration >= miniter:
+                if abs(qoi - qoi_old) < qoi_rtol * abs(qoi_old):
+                    converged_reason = "QoI convergence"
+                    break
+            qoi_old = qoi
+
+            # Check for error estimator convergence
+            estimator = dwr.vector().gather().sum()
+            print(f"      Error estimator      = {estimator}")
+            if estimator_old is not None and fp_iteration >= miniter:
+                if abs(estimator - estimator_old) < estimator_rtol * abs(estimator_old):
+                    converged_reason = "error estimator convergence"
+                    break
+            estimator_old = estimator
+
+            # Process metric
+            P1_ten = TensorFunctionSpace(mesh, "CG", 1)
+            p1metric = hessian_metric(clement_interpolant(p0metric))
+            space_normalise(p1metric, target_ramp, "inf")
+            enforce_element_constraints(
+                p1metric, setup.parameters.h_min, setup.parameters.h_max, 1.0e05
+            )
+
+            # Adapt the mesh and check for element count convergence
+            metric = RiemannianMetric(mesh)
+            metric.assign(p1metric)
+            mesh = adapt(mesh, metric)
+            print(f"    Mesh {fp_iteration+1}")
+            if fp_iteration >= miniter:
+                if abs(mesh.num_cells() - elements_old) < element_rtol * abs(elements_old):
+                    converged_reason = "element count convergence"
+                    break
+            elements_old = mesh.num_cells()
+            print(f"      Element count        = {elements_old}")
+
+            # Check for reaching maximum number of iterations
+            if fp_iteration == maxiter:
+                converged_reason = "reaching maximum iteration count"
+        print(f"    Terminated after {fp_iteration+1} iterations due to {converged_reason}")
+        times.append(perf_counter() - cpu_timestamp)
+        qois.append(qoi)
+        dofs.append(dof)
+        elements.append(elements_old)
+        estimators.append(estimator)
+        niter.append(fp_iteration + 1)
+        np.save(f"{model}/data/qois_GO{approach}_{test_case}", qois)
+        np.save(f"{model}/data/dofs_GO{approach}_{test_case}", dofs)
+        np.save(f"{model}/data/elements_GO{approach}_{test_case}", elements)
+        np.save(f"{model}/data/estimators_GO{approach}_{test_case}", estimators)
+        np.save(f"{model}/data/times_GO{approach}_{test_case}", times)
+        np.save(f"{model}/data/niter_GO{approach}_{test_case}", niter)
+    except ConvergenceError:
+        print("Skipping due to convergence error")
+        continue
--- examples/run_adaptation_loop_ml.py
+++ examples/run_adaptation_loop_ml.py
@@ -58,104 +58,108 @@ nn.eval()
 qois, dofs, elements, times, niter = [], [], [], [], []
 print(f"Test case {test_case}")
 for i in range(num_refinements + 1):
-    target_complexity = 200.0 * 4**i
-    mesh = Mesh(f"{model}/meshes/{test_case}.msh")
-    qoi_old = None
-    elements_old = mesh.num_cells()
-    estimator_old = None
-    converged_reason = None
-    print(f"  Target {target_complexity}\n    Mesh 0")
-    print(f"      Element count        = {elements_old}")
-    cpu_timestamp = perf_counter()
-    for fp_iteration in range(maxiter + 1):
-
-        # Ramp up the target complexity
-        target_ramp = ramp_complexity(200.0, target_complexity, fp_iteration)
-
-        # Solve forward and adjoint and compute Hessians
-        fwd_sol, adj_sol = get_solutions(mesh, setup)
-        P0 = FunctionSpace(mesh, "DG", 0)
-        P0_ten = TensorFunctionSpace(mesh, "DG", 0)
-
-        # Check for QoI convergence
-        qoi = assemble(setup.get_qoi(mesh)(fwd_sol))
-        print(f"      Quantity of Interest = {qoi} {unit}")
-        if qoi_old is not None and fp_iteration >= miniter:
-            if abs(qoi - qoi_old) < qoi_rtol * abs(qoi_old):
-                converged_reason = "QoI convergence"
-                break
-        qoi_old = qoi
-
-        # Extract features
-        features = collect_features(
-            extract_features(setup, fwd_sol, adj_sol, preproc=preproc)
-        )
-
-        # Run model
-        test_targets = np.array([])
-        with torch.no_grad():
-            for i in range(features.shape[0]):
-                test_x = torch.Tensor(features[i]).to(device)
-                test_prediction = nn(test_x)
-                test_targets = np.concatenate(
-                    (test_targets, np.array(test_prediction.cpu()))
-                )
-        dwr = Function(P0)
-        dwr.dat.data[:] = np.abs(test_targets)
-
-        # Check for error estimator convergence
-        estimator = dwr.vector().gather().sum()
-        print(f"      Error estimator      = {estimator}")
-        if estimator_old is not None and fp_iteration >= miniter:
-            if abs(estimator - estimator_old) < estimator_rtol * abs(estimator_old):
-                converged_reason = "error estimator convergence"
-                break
-        estimator_old = estimator
-
-        # Construct metric
-        if approach == "anisotropic":
-            hessian = combine_metrics(*get_hessians(fwd_sol), average=False)
-        else:
-            hessian = None
-        p0metric = anisotropic_metric(
-            dwr,
-            hessian,
-            target_complexity=target_ramp,
-            target_space=P0_ten,
-            interpolant="L2",
-        )
-
-        # Process metric
-        P1_ten = TensorFunctionSpace(mesh, "CG", 1)
-        p1metric = hessian_metric(clement_interpolant(p0metric))
-        space_normalise(p1metric, target_ramp, "inf")
-        enforce_element_constraints(
-            p1metric, setup.parameters.h_min, setup.parameters.h_max, 1.0e05
-        )
-
-        # Adapt the mesh and check for element count convergence
-        metric = RiemannianMetric(mesh)
-        metric.assign(p1metric)
-        mesh = adapt(mesh, metric)
-        print(f"    Mesh {fp_iteration+1}")
-        if fp_iteration >= miniter:
-            if abs(mesh.num_cells() - elements_old) < element_rtol * abs(elements_old):
-                converged_reason = "element count convergence"
-                break
+    try:
+        target_complexity = 200.0 * 4**i
+        mesh = Mesh(f"{model}/meshes/{test_case}.msh")
+        qoi_old = None
         elements_old = mesh.num_cells()
+        estimator_old = None
+        converged_reason = None
+        print(f"  Target {target_complexity}\n    Mesh 0")
         print(f"      Element count        = {elements_old}")
+        cpu_timestamp = perf_counter()
+        for fp_iteration in range(maxiter + 1):
 
-        # Check for reaching maximum number of iterations
-        if fp_iteration == maxiter:
-            converged_reason = "reaching maximum iteration count"
-    print(f"    Terminated after {fp_iteration+1} iterations due to {converged_reason}")
-    times.append(perf_counter() - cpu_timestamp)
-    qois.append(qoi)
-    dofs.append(sum(fwd_sol.function_space().dof_count))
-    elements.append(elements_old)
-    niter.append(fp_iteration + 1)
-    np.save(f"{model}/data/qois_ML{approach}_{test_case}", qois)
-    np.save(f"{model}/data/dofs_ML{approach}_{test_case}", dofs)
-    np.save(f"{model}/data/elements_ML{approach}_{test_case}", elements)
-    np.save(f"{model}/data/times_ML{approach}_{test_case}", times)
-    np.save(f"{model}/data/niter_ML{approach}_{test_case}", niter)
+            # Ramp up the target complexity
+            target_ramp = ramp_complexity(200.0, target_complexity, fp_iteration)
+
+            # Solve forward and adjoint and compute Hessians
+            fwd_sol, adj_sol = get_solutions(mesh, setup)
+            P0 = FunctionSpace(mesh, "DG", 0)
+            P0_ten = TensorFunctionSpace(mesh, "DG", 0)
+
+            # Check for QoI convergence
+            qoi = assemble(setup.get_qoi(mesh)(fwd_sol))
+            print(f"      Quantity of Interest = {qoi} {unit}")
+            if qoi_old is not None and fp_iteration >= miniter:
+                if abs(qoi - qoi_old) < qoi_rtol * abs(qoi_old):
+                    converged_reason = "QoI convergence"
+                    break
+            qoi_old = qoi
+
+            # Extract features
+            features = collect_features(
+                extract_features(setup, fwd_sol, adj_sol, preproc=preproc)
+            )
+
+            # Run model
+            test_targets = np.array([])
+            with torch.no_grad():
+                for i in range(features.shape[0]):
+                    test_x = torch.Tensor(features[i]).to(device)
+                    test_prediction = nn(test_x)
+                    test_targets = np.concatenate(
+                        (test_targets, np.array(test_prediction.cpu()))
+                    )
+            dwr = Function(P0)
+            dwr.dat.data[:] = np.abs(test_targets)
+
+            # Check for error estimator convergence
+            estimator = dwr.vector().gather().sum()
+            print(f"      Error estimator      = {estimator}")
+            if estimator_old is not None and fp_iteration >= miniter:
+                if abs(estimator - estimator_old) < estimator_rtol * abs(estimator_old):
+                    converged_reason = "error estimator convergence"
+                    break
+            estimator_old = estimator
+
+            # Construct metric
+            if approach == "anisotropic":
+                hessian = combine_metrics(*get_hessians(fwd_sol), average=False)
+            else:
+                hessian = None
+            p0metric = anisotropic_metric(
+                dwr,
+                hessian,
+                target_complexity=target_ramp,
+                target_space=P0_ten,
+                interpolant="L2",
+            )
+
+            # Process metric
+            P1_ten = TensorFunctionSpace(mesh, "CG", 1)
+            p1metric = hessian_metric(clement_interpolant(p0metric))
+            space_normalise(p1metric, target_ramp, "inf")
+            enforce_element_constraints(
+                p1metric, setup.parameters.h_min, setup.parameters.h_max, 1.0e05
+            )
+
+            # Adapt the mesh and check for element count convergence
+            metric = RiemannianMetric(mesh)
+            metric.assign(p1metric)
+            mesh = adapt(mesh, metric)
+            print(f"    Mesh {fp_iteration+1}")
+            if fp_iteration >= miniter:
+                if abs(mesh.num_cells() - elements_old) < element_rtol * abs(elements_old):
+                    converged_reason = "element count convergence"
+                    break
+            elements_old = mesh.num_cells()
+            print(f"      Element count        = {elements_old}")
+
+            # Check for reaching maximum number of iterations
+            if fp_iteration == maxiter:
+                converged_reason = "reaching maximum iteration count"
+        print(f"    Terminated after {fp_iteration+1} iterations due to {converged_reason}")
+        times.append(perf_counter() - cpu_timestamp)
+        qois.append(qoi)
+        dofs.append(sum(fwd_sol.function_space().dof_count))
+        elements.append(elements_old)
+        niter.append(fp_iteration + 1)
+        np.save(f"{model}/data/qois_ML{approach}_{test_case}", qois)
+        np.save(f"{model}/data/dofs_ML{approach}_{test_case}", dofs)
+        np.save(f"{model}/data/elements_ML{approach}_{test_case}", elements)
+        np.save(f"{model}/data/times_ML{approach}_{test_case}", times)
+        np.save(f"{model}/data/niter_ML{approach}_{test_case}", niter)
+    except ConvergenceError:
+        print("Skipping due to convergence error")
+        continue
