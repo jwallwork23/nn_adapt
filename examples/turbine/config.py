93d082dd50f7c656b3207e7df85bc40ca208302a
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-06-17 15:19:41 +0100

    turbine: fix config

--- examples/turbine/config.py
+++ examples/turbine/config.py
@@ -37,51 +37,18 @@ def initialise(case, discrete=False):
     """
     parameters.case = case
     parameters.discrete = discrete
-    if "aligned" in case:
-        parameters.viscosity_coefficient = 0.5
-        parameters.depth = 40.0
-        parameters.inflow_speed = 5.0
-        parameters.turbine_coords = [(456, 250), (744, 250)]
-    elif "offset" in case:
-        parameters.viscosity_coefficient = 0.5
-        parameters.depth = 40.0
-        parameters.inflow_speed = 5.0
-        parameters.turbine_coords = [(456, 232), (744, 268)]
-    elif "unseen" in case:
-        parameters.viscosity_coefficient = 2.0
-        parameters.depth = 200.0
-        parameters.inflow_speed = 10.0
-        parameters.turbine_coords = [(456, 232), (744, 268)]
-    elif "pipe" in case:
-
-        def inflow(mesh):
-            x, y = SpatialCoordinate(mesh)
-            yy = y / 200
-            return as_vector([yy ** 2 * (1 - yy ** 2), 0])
-
-        parameters.viscosity_coefficient = 100.0
-        parameters.depth = 50.0
-        parameters.u_inflow = inflow
-        parameters.ic = lambda mesh: as_vector([1.0, 0.0])
-        parameters.turbine_coords = [(500, 250), (700, 440)]
-        parameters.wall_boundary_type = "uv"
-        parameters.qoi_unit = "kW"
-        parameters.density = Constant(1030.0 * 1.0e-03)
-    else:
-        assert isinstance(case, int)
+    if isinstance(case, int):
         parameters.turbine_coords = []
         np.random.seed(100 * case)
 
         # Random depth from 20m to 100m
-        parameters.depth = sample_uniform(20.0, 10.0)
-        # TODO: spatially varying
+        parameters.depth = sample_uniform(20.0, 100.0)
 
         # Random inflow speed from 0.5 m/s to 6 m/s
         parameters.inflow_speed = sample_uniform(0.5, 6.0)
 
         # Random viscosity from 0.01 m^2/s to 10 m^2/s
         parameters.viscosity_coefficient = sample_uniform(0.1, 1.0)
-        # TODO: spatially varying
 
         # Randomise turbine configuration such that all
         # turbines are at least 50m from the domain
@@ -99,6 +66,41 @@ def initialise(case, discrete=False):
             if valid:
                 tc.append((x, y))
                 i += 1
+        return
+    elif "aligned" in case:
+        parameters.viscosity_coefficient = 0.5
+        parameters.depth = 40.0
+        parameters.inflow_speed = 5.0
+        parameters.turbine_coords = [(456, 250), (744, 250)]
+    elif "offset" in case:
+        parameters.viscosity_coefficient = 0.5
+        parameters.depth = 40.0
+        parameters.inflow_speed = 5.0
+        parameters.turbine_coords = [(456, 232), (744, 268)]
+    elif "unseen" in case:
+        parameters.viscosity_coefficient = 2.0
+        parameters.depth = 200.0
+        parameters.inflow_speed = 10.0
+        parameters.turbine_coords = [(456, 232), (744, 268)]
+    elif "pipe" in case:
+        u_in = Constant(5.0)
+        parameters.inflow_speed = u_in
+        w = Constant(500.0)
+
+        def inflow(mesh):
+            y = SpatialCoordinate(mesh)[1] / w
+            return as_vector([u_in * y ** 2 * (1 - y) ** 2, 0])
+
+        parameters.viscosity_coefficient = 20.0
+        parameters.depth = 40.0
+        parameters.u_inflow = inflow
+        parameters.ic = lambda mesh: as_vector([u_in, 0.0])
+        parameters.turbine_coords = [(450, 350), (650, 450)]
+        parameters.wall_boundary_type = "uv"
+        parameters.qoi_unit = "kW"
+        parameters.density = Constant(1030.0 * 1.0e-03)
+    else:
+        raise ValueError(f"Test case {test_case} not recognised")
 
     if "reversed" in case:
         parameters.inflow_speed *= -1
