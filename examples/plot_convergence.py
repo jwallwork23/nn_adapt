261170b266afaed3677c10dfcfa78cadfc589405
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-06-22 10:06:14 +0100

    examples: conv_approach for plot_convergence

--- examples/plot_convergence.py
+++ examples/plot_convergence.py
@@ -42,24 +42,22 @@ approaches = {
     },
 }
 xlim = {
-    "dofs": [3.0e03, 4.0e06],
-    "elements": [3.0e02, 4.0e05],
+    "dofs": [3.0e03, 3.0e06],
     "times": [1.0e0, 2.0e03],
 }
+conv_approach = "uniform"
 
 # Load configuration
 setup = importlib.import_module(f"{model}.config")
+setup.initialise(test_case)
 unit = setup.parameters.qoi_unit
 qoi_name = setup.parameters.qoi_name.capitalize()
 
 # Load outputs
-dofs, elements, qois, times, niter = {}, {}, {}, {}, {}
+dofs, qois, times, niter = {}, {}, {}, {}
 for approach in approaches.copy():
     try:
         dofs[approach] = np.load(f"{model}/data/dofs_{approach}_{test_case}.npy")
-        elements[approach] = np.load(
-            f"{model}/data/elements_{approach}_{test_case}.npy"
-        )
         qois[approach] = np.load(f"{model}/data/qois_{approach}_{test_case}.npy")
         times[approach] = np.load(f"{model}/data/times_{approach}_{test_case}.npy")
         niter[approach] = np.load(f"{model}/data/niter_{approach}_{test_case}.npy")
@@ -68,6 +66,13 @@ for approach in approaches.copy():
         print(f"Cannot load {approach} data for test case {test_case}")
         approaches.pop(approach)
         continue
+
+    # Drop first iteration because timings include compilation   # FIXME: Why?
+    if approach != conv_approach:
+        dofs[approach] = dofs[approach][1:]
+        qois[approach] = qois[approach][1:]
+        times[approach] = times[approach][1:]
+        niter[approach] = niter[approach][1:]
 if len(approaches.keys()) == 0:
     print("Nothing to plot.")
     sys.exit(0)
@@ -75,13 +80,13 @@ if len(approaches.keys()) == 0:
 # Plot QoI curves against DoF count
 fig, axes = plt.subplots()
 start = max(np.load(f"{model}/data/qois_uniform_{test_case}.npy"))
-conv = 19.82480044 if test_case == "aligned" else 23.17004884  # TODO: avoid hard-code
-# conv = np.load(f"{model}/data/qois_GOanisotropic_{test_case}.npy")[-1]
+conv = np.load(f"{model}/data/qois_{conv_approach}_{test_case}.npy")[-1]
 axes.hlines(conv, *xlim["dofs"], "k", label="Converged QoI")
 for approach, metadata in approaches.items():
     axes.semilogx(dofs[approach], qois[approach], **metadata)
 axes.set_xlim(xlim["dofs"])
-axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
+if test_case in ["aligned", "offset"]:
+    axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
 axes.yaxis.set_major_formatter(FormatStrFormatter("%.2f"))
 axes.set_xlabel("DoF count")
 axes.set_ylabel(qoi_name + r" ($\mathrm{" + unit + r"}$)")
@@ -97,7 +102,8 @@ for approach, metadata in approaches.items():
     for n, t, q in zip(niter[approach], times[approach], qois[approach]):
         axes.annotate(str(n), (1.1 * t, q), color=metadata["color"], fontsize=14)
 axes.set_xlim(xlim["times"])
-axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
+if test_case in ["aligned", "offset"]:
+    axes.set_ylim([conv - 0.05 * (start - conv), start + 0.05 * (start - conv)])
 axes.yaxis.set_major_formatter(FormatStrFormatter("%.2f"))
 axes.set_xlabel(r"CPU time ($\mathrm{s}$)")
 axes.set_ylabel(qoi_name + r" ($\mathrm{" + unit + "}$)")
@@ -106,13 +112,33 @@ plt.tight_layout()
 plt.savefig(f"{model}/plots/qoi_vs_cputime_{test_case}.pdf")
 plt.close()
 
+# Plot CPU time curves against DoF count
+fig, axes = plt.subplots()
+for approach, metadata in approaches.items():
+    axes.loglog(dofs[approach], times[approach], **metadata)
+    for n, t, d in zip(niter[approach], times[approach], dofs[approach]):
+        axes.annotate(str(n), (1.1 * d, t), color=metadata["color"], fontsize=14)
+axes.set_xlabel("DoF count")
+axes.set_ylabel(r"CPU time ($\mathrm{s}$)")
+axes.set_xlim(xlim["dofs"])
+axes.set_ylim(xlim["times"])
+axes.grid(True, which="both")
+plt.tight_layout()
+plt.savefig(f"{model}/plots/cputime_vs_dofs_{test_case}.pdf")
+plt.close()
+
+if conv_approach is None:
+    sys.exit(0)
+qois[conv_approach] = qois[conv_approach][:-1]
+dofs[conv_approach] = dofs[conv_approach][:-1]
+times[conv_approach] = times[conv_approach][:-1]
+
 # Plot QoI error curves against DoF count
 errors = {}
 fig, axes = plt.subplots()
 for approach, metadata in approaches.items():
     errors[approach] = np.abs((qois[approach] - conv) / conv)
     axes.loglog(dofs[approach], errors[approach], **metadata)
-axes.set_xlim(xlim["dofs"])
 axes.set_xlabel("DoF count")
 axes.set_ylabel(r"QoI error ($\%$)")
 axes.grid(True, which="both")
@@ -137,25 +163,9 @@ for approach, metadata in approaches.items():
     axes.loglog(times[approach], errors[approach], **metadata)
     for n, t, e in zip(niter[approach], times[approach], errors[approach]):
         axes.annotate(str(n), (1.1 * t, e), color=metadata["color"], fontsize=14)
-axes.set_xlim(xlim["times"])
 axes.set_xlabel(r"CPU time ($\mathrm{s}$)")
 axes.set_ylabel(r"QoI error ($\%$)")
 axes.grid(True, which="both")
 plt.tight_layout()
 plt.savefig(f"{model}/plots/qoi_error_vs_cputime_{test_case}.pdf")
-plt.close()
-
-# Plot CPU time curves against DoF count
-fig, axes = plt.subplots()
-for approach, metadata in approaches.items():
-    axes.loglog(dofs[approach], times[approach], **metadata)
-    for n, t, d in zip(niter[approach], times[approach], dofs[approach]):
-        axes.annotate(str(n), (1.1 * d, t), color=metadata["color"], fontsize=14)
-axes.set_xlabel("DoF count")
-axes.set_ylabel(r"CPU time ($\mathrm{s}$)")
-axes.set_xlim(xlim["dofs"])
-axes.set_ylim(xlim["times"])
-axes.grid(True, which="both")
-plt.tight_layout()
-plt.savefig(f"{model}/plots/cputime_vs_dofs_{test_case}.pdf")
 plt.close()
