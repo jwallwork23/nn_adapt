1d032a5e07a78360efecdaf30c97bffb0592146a
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-07-06 14:20:28 +0100

    examples: compute sensitivity using mean

--- examples/compute_importance.py
+++ examples/compute_importance.py
@@ -66,10 +66,10 @@ nn.eval()
 loss_fn = Loss()
 
 # Compute (averaged) sensitivities of the network to the inputs
-sensitivity = torch.zeros(layout.num_inputs)
-count = 0
+dJdm = torch.zeros(layout.num_inputs)
 data_dir = f"{model}/data"
 approaches = parsed_args.approaches
+values = np.zeros((0, layout.num_inputs))
 for step in range(parsed_args.adaptation_steps):
     for approach in approaches:
         for test_case in range(1, parsed_args.num_training_cases + 1):
@@ -82,17 +82,21 @@ for step in range(parsed_args.adaptation_steps):
                 key: np.load(f"{data_dir}/feature_{key}_{suffix}.npy")
                 for key in layout.inputs
             }
-            target = np.load(f"{data_dir}/target_{suffix}.npy")
-            features = torch.from_numpy(collect_features(data)).type(torch.float32)
-            expected = torch.from_numpy(target).type(torch.float32)
+            features = collect_features(data)
+            values = np.vstack((values, features))
+            features = torch.from_numpy(features).type(torch.float32)
             features.requires_grad_(True)
 
-            # Evaluate the loss
-            loss = loss_fn(expected, nn(features))
+            # Run the model and sum the outputs
+            out = nn(features).sum(axis=0)
 
-            # Backpropagate and average to get the sensitivities
-            loss.backward()
-            sensitivity += features.grad.abs().mean(axis=0)
-            count += 1
-sensitivity /= count
+            # Backpropagate to get the gradient of the outputs w.r.t. the inputs
+            out.backward()
+            dJdm += features.grad.mean(axis=0)
+
+# Compute representative values for each parameter
+dm = np.abs(np.mean(values, axis=0))
+
+# Multiply by the variability
+sensitivity = dJdm.abs().detach().numpy() * dm
 np.save(f"{model}/data/sensitivities_{tag}.npy", sensitivity)
